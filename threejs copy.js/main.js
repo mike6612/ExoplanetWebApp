844b037ca52dec42d713659638fb48daa02695da
mike6612 <michaeljaimes66@gmail.com>
2024-12-25 23:31:36 -0600

    update

---
+++ threejs copy.js/main.js
@@ -0,0 +1,52 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x808080);
+
+const loader = new GLTFLoader();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; // Smooth controls
+controls.dampingFactor = 0.05;
+
+loader.load("scene.gltf", (gltf) => {
+  scene.add(gltf.scene);
+  gltf.scene.position.set(0, 0, 0);
+  gltf.scene.scale.set(25, 25, 25);
+});
+
+loader.load("000.gltf", (gltf) => {
+  scene.add(gltf.scene);
+
+  gltf.scene.position.set(0, 0, 0);
+
+  gltf.scene.scale.set(20, 20, 20);
+});
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
