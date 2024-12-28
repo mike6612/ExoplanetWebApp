4e87d45649d5b3b20d98b5e7c13edad222f24e3a
mike6612 <michaeljaimes66@gmail.com>
2024-12-28 03:22:06 -0600

    updating

--- threejs copy.js/main.js
+++ threejs copy.js/main.js
@@ -1,31 +1,35 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-
+let textureCube;
 const scene = new THREE.Scene();
 // scene.background = new THREE.Color(0x808080);
 
 const loader2 = new THREE.CubeTextureLoader();
 
 textureCube = loader2.load([
-  "right.jpg",
-  "left.jpg",
-  "top.jpg",
-  "bottom.jpg",
-  "front.jpg",
-  "back.jpg",
+  "right5.png",
+  "left5.png",
+  "top5.png",
+  "bottom5.png",
+  "front5.png",
+  "back5.png",
 ]);
+
+scene.background = textureCube;
+
 const loader = new GLTFLoader();
 
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000
+  7000
 );
-camera.position.z = 5;
+camera.position.z = 6000;
 
 const renderer = new THREE.WebGLRenderer();
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -33,7 +37,7 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true; // Smooth controls
 controls.dampingFactor = 0.05;
 
-loader.load("000.gltf", (gltf) => {
+loader.load("001.gltf", (gltf) => {
   scene.add(gltf.scene);
 
   gltf.scene.position.set(0, 0, 0);
